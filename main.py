52adf822a29a50617a784637576c984bda9cfb4c
Stas <127448305+yourcatwastaken@users.noreply.github.com>
2026-01-19 12:58:06 +0000

    refactor: move time calculation to separate function outside of the main loop

--- main.py
+++ main.py
@@ -12,11 +12,30 @@ options = Options()
 options.add_argument('--headless=new')
 driver = webdriver.Chrome(options=options)
 
+def calculate_outage_times(time_range, current_time):
+    start_time_str = time_range.split('-')[0]
+    end_time_str = time_range.split('-')[1]
+    today_start = datetime.strptime(start_time_str, '%H:%M')
+    today_end = datetime.strptime(end_time_str, '%H:%M')
+    outage_time = current_time.replace(hour=today_start.hour, minute=today_start.minute, second=0)
+    end_time = current_time.replace(hour=today_end.hour, minute=today_end.minute, second=0)
+    if end_time_str == '00:00':
+        end_time += timedelta(days=1)
+    mins_until_start = (outage_time - current_time).total_seconds() / 60
+    mins_until_end = (end_time - current_time).total_seconds() / 60
+    return outage_time, end_time, mins_until_start, mins_until_end, start_time_str, end_time_str
+
+
+def alert_sound():
+    for _ in range(5):
+        winsound.Beep(1500, 500)
+
+
 try:
     while True:
         try:
-            now = datetime.now()
-            print(f"Checking schedule at: {now.strftime('%H:%M')}")
+            current_time = datetime.now()
+            print(f"Checking schedule at: {current_time.strftime('%H:%M')}")
 
             with open('config.json') as file:
                 config_data = json.load(file)
@@ -37,7 +56,7 @@ try:
 
             if target_index is None:
                 print(f'Error: Could not find queue {user_queue} on the page.')
-                time.sleep(600)
+                time.sleep(60)
                 continue
 
             rows = wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'tabSchedule-row')))
@@ -45,30 +64,18 @@ try:
                 td = row.find_elements(By.TAG_NAME, 'td')
                 if target_index is not None and len(td) > target_index:
                     time_range = td[0].text
-                    start_time_str = time_range.split('-')[0]
-                    end_time_str = time_range.split('-')[1]
-                    today_start = datetime.strptime(start_time_str, '%H:%M')
-                    today_end = datetime.strptime(end_time_str, '%H:%M')
-                    outage_time = now.replace(hour=today_start.hour, minute=today_start.minute, second=0)
-                    end_time = now.replace(hour=today_end.hour, minute=today_end.minute, second=0)
-                    if end_time_str == '00:00':
-                        end_time += timedelta(days=1)
-
-                    minutes_until_start = (outage_time - now).total_seconds() / 60
-                    minutes_until_end = (end_time - now).total_seconds() / 60
-
+                    out_ts, end_ts, m_start, m_end, start_str, end_str = calculate_outage_times(time_range, current_time)
                     status = td[target_index].get_attribute('class')
                     if 'item-probably' in status or 'item-off' in status:
-                        if outage_time <= now < end_time:
-                            h, m = divmod(int(minutes_until_end), 60)
+                        if out_ts <= current_time < end_ts:
+                            h, m = divmod(int(m_end), 60)
                             time_text = f'{h}h {m}m' if h > 0 else f'{m}m'
-                            print(f'!!! STATUS: Power is currently off. Ends in {time_text} (at {end_time_str}) !!!')
+                            print(f'!!! STATUS: Power is currently off. Ends in {time_text} (at {end_str}) !!!')
                             break
-                        elif 0 < minutes_until_start <= alert_window:
-                            print(f'!!! ALERT: Outage starting in {int(minutes_until_start)} minutes (at {start_time_str}) !!!')
+                        elif 0 < m_start <= alert_window:
+                            print(f'!!! ALERT: Outage starting in {int(m_start)} minutes (at {start_str}) !!!')
                             if sound_enabled:
-                                for i in range(5):
-                                    winsound.Beep(1500, 500)
+                                alert_sound()
                             break
             time.sleep(600)
 
